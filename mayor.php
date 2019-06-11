004f5c6240030c5c8f399fa73ae0446a162e79e3
brunoandres <brunoandres2013@gmail.com>
2019-06-11 16:26:01 -0300

    export data

--- mayor.php
+++ mayor.php
@@ -185,13 +185,13 @@ include("menu.php");
 </div>
 <script type="text/javascript" src="jquery/jquery-1.10.1.min.js"></script>
 <script type="text/javascript" language="javascript" src="jquery/jquery.dataTables.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script src="js/buttons/jquery-3.3.1.js"></script>
+<script src="js/buttons/jquery.dataTables.min.js"></script>
+<script src="js/buttons/dataTables.buttons.min.js"></script>
+<script src="js/buttons/jszip.min.js"></script>
+<script src="js/buttons/pdfmake.min.js"></script>
+<script src="js/buttons/vfs_fonts.js"></script>
+<script src="js/buttons/buttons.html5.min.js"></script>
 
 <script type="text/javascript" language="javascript" class="init">
   $(document).ready(function() {
