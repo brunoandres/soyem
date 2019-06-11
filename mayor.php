33b82e2dbd45867725bd9739242c4a022443dc5c
brunoandres <brunoandres2013@gmail.com>
2019-06-11 01:16:06 -0300

    Agrego botones de exportar en excel o pdf la tabla de mayor con sus respectivos valores totales de debe, haber y saldo

--- mayor.php
+++ mayor.php
@@ -185,15 +185,45 @@ include("menu.php");
 </div>
 <script type="text/javascript" src="jquery/jquery-1.10.1.min.js"></script>
 <script type="text/javascript" language="javascript" src="jquery/jquery.dataTables.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+
 <script type="text/javascript" language="javascript" class="init">
   $(document).ready(function() {
-  
-    $('#mayor').DataTable({
-    	"pageLength":50,
-      	"language": {
-          "url": "spanish.json"
-        }
-    });
+    $('#mayor').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3 ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3 ]
+                }
+            },
+            {
+                extend: 'csvHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3 ]
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3 ]
+                }
+            }
+        ]
+    } );
 } );
 </script>
 </body>
