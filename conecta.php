eab8a89bd3a58f32238395c9acea0e85a43ecfb5
Your name here <brunoandres2013@gmail.com>
2019-08-05 23:09:27 -0300

    archivo conecta configurando servidor cenergon

--- conecta.php
+++ conecta.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-$conn=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
+$conn=mysql_connect ("mysql.hostinger.com.ar", "u359426750_soyem", "sistemasoyem2019") or die ('I cannot connect to the database because: ' . mysql_error());
 mysql_set_charset('utf8',$conn);
-mysql_select_db ("soyem");
+mysql_select_db ("u359426750_soyem");
 ?>
