ac2379e5d920cf4fbb2312943c24a0e4d5fafed1
brunoandres <brunoandres2013@gmail.com>
2020-01-20 21:27:31 -0300

    corrijo menu

--- recortes/menu_cont.php
+++ recortes/menu_cont.php
@@ -3,6 +3,6 @@
  -  <a href="ejercicios.php" title="manejar los ejercicios contables" <?php if($subpage=='ejercicios'){echo 'style=color:red;';}else{echo 'item';} ?>>Ejercicios</a>
   -  <a href="deuda_prestamos.php" title="deuda de prestamos descuento" <?php if($subpage=='deuda_prestamo'){echo 'style=color:red;';}else{echo 'item';} ?>>Deuda Prestamo</a>
 
-  -  <a href="subir_comprobante.php" title="Subir archivo de comprobantes" <?php if($subpage=='subir_comprobante'){echo 'style=color:red;';}else{echo 'item';} ?>>Subir Archivo</a>
+  -  <a href="subir_comprobante.php" title="Subir archivo de comprobantes" <?php if($subpage=='subir_comprobante'){echo 'style=color:red;';}else{echo 'item';} ?>>Subir Archivo Banco</a>
  </b>
 </div>
